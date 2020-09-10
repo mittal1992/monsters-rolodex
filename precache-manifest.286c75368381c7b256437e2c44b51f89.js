cd96bfd5bee524a7e845e2d77143212fbd906c53
LAPTOP-R8TDI5FV\sidha <sidharthmittal1992@outlook.com>
2020-09-10 15:56:41 -0500

    Updates

---
+++ precache-manifest.286c75368381c7b256437e2c44b51f89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e99aaeefd27373c3757bcb2c33824267",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d72e86c7ce21e6e54677",
+    "url": "/monsters-rolodex/static/css/main.fefac440.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d72e86c7ce21e6e54677",
+    "url": "/monsters-rolodex/static/js/main.4c780a27.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
